58a52bdecf979340fcbd0d99262321fa00248e5a
openhardnudd <hard_nudd@126.com>
2019-05-18 14:29:40 +0800

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+package com.vipsolve.learn.semux.core;
+
+import org.junit.Test;
+
+/**
+ * Title:
+ * Description:
+ * Copyright: Copyright (c)2019
+ * Company: alienchain
+ *
+ * @author cyench
+ */
+public class BlockTest {
+
+    @Test
+    public void driftTest() {
+        System.out.println( 1<<0 );
+        System.out.println( 1<<1 );
+        System.out.println( 1<<2 );
+        System.out.println( 1<<3 );
+
+        System.out.println(0x01);
+        System.out.println(0x20);
+        System.out.println(0x24);
+        System.out.println(0x44);
+    }
+}
